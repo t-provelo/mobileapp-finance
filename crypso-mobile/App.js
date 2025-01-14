2054b77827ce58f0f30260a0ee5f85b37d52e6b9
Terry Tennard <terrytennardjr@gmail.com>
2025-01-14 11:50:24 -0600

    Updated App.js, added navigation and screens directories, and included package-lock.json

--- crypso-mobile/App.js
+++ crypso-mobile/App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import AppNavigator from './navigation/AppNavigator'; // Path to your AppNavigator
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" /> {/* Set status bar style */}
+      <AppNavigator /> {/* This will render your app's navigation */}
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
