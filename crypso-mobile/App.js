5f6efa602cd1175edbf48e15b1f362fb59393a83
Terry Tennard <terrytennardjr@gmail.com>
2025-01-29 12:11:04 -0600

    Update all files and code

--- crypso-mobile/App.js
+++ crypso-mobile/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './AppNavigator';
+import AppNavigator from './navigation/AppNavigator';
+
 
 export default function App() {
   return (
