d52970e91668df237c4293f51151a1ab74359746
Terry Tennard <terrytennardjr@gmail.com>
2025-02-19 11:47:18 -0600

    Update code

--- crypso-mobile/index.js
+++ crypso-mobile/index.js
@@ -1,10 +1,4 @@
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './App';
-import { name as appName } from './app.json';
 
-// Manually define setImmediate if missing
-if (typeof global.setImmediate === 'undefined') {
-  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
-}
-
-AppRegistry.registerComponent(appName, () => App);
+registerRootComponent(App);
